fd67d28b8b1373cbe719edd1b4de384a559b6c69
Dallas3 <dallasgordon3@gmail.com>
2025-12-10 15:27:33 -0800

    Balloon fetching setup

---
+++ fetchBalloons.js
@@ -0,0 +1,38 @@
+// Fetch balloon data via provided API
+const API_URL = 'https://a.windbornesystems.com/treasure/00.json'
+
+async function loadBalloonData() {
+  // Try/Catch because this API might get wonky
+  try {
+    const res = await fetch(API_URL)
+    const data = await res.json()
+
+    // Test showing with 10 balloons first
+    const balloonData = data.slice(0, 10)
+    const balloonDisplay = document.getElementById('balloons-container')
+
+    balloonData.forEach((item, index) => {
+      // Assuming 3rd value is Altitude, not sure of metric, freedom units, bathtubs
+      const [lat, long, alt] = item
+
+      const div = document.createElement('div')
+      div.className = 'balloon'
+
+      // 99 Red balloons worth of data
+      div.innerHTML = `
+        <h2>Balloon ID:${index + 1}</h2>
+        <p><strong>Latitude</strong> ${lat}</p>
+        <p><strong>Longitude</strong> ${long}</p>
+        <p><strong>Altitude</strong> ${alt}</p>
+      `
+      balloonDisplay.appendChild(div)
+    })
+
+    
+  } catch {
+    console.error('Issue fetching balloon data:', error)
+  }
+
+  // Run on page load, future update will be to manipulate timing
+  loadBalloonData()
+}
