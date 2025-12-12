cb9007e6622242b3f24a08440044554d1d59ae45
Dallas3 <dallasgordon3@gmail.com>
2025-12-11 16:04:04 -0800

    React build started

---
+++ balloon-test/balloon-tracker/src/components/BalloonCard.jsx
@@ -0,0 +1,22 @@
+function BalloonCard({balloon}) {
+  
+  function onTrackClick() {
+    alert(`tracking: ${balloon.name}`)
+  }
+  
+  return (
+    <div className="balloon-card">
+      <div className="balloon-info">
+        <h1 className="balloon-name">{balloon.name}</h1>
+        <p className="balloon-lat">{balloon.lat}</p>
+        <p className="balloon-long">{balloon.long}</p>
+        <p className="balloon-alt">{balloon.alt}</p>
+        <button className="balloon-track" onClick={onTrackClick}>
+          Track Balloon
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default BalloonCard
