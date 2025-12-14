4852265ca6087513ce046aacbbc778d0d034c060
Dallas3 <dallasgordon3@gmail.com>
2025-12-13 17:33:47 -0800

    Github building stuffs updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       // any request to /api/* will be forwarded to the remote host
