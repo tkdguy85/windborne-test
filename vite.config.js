06661826810916c42804f24f3b8c6bc72a9f5c46
Dallas3 <dallasgordon3@gmail.com>
2025-12-13 17:34:41 -0800

    Updated path, because you know

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/windborne-test/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
